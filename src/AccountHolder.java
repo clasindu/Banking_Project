2a1259680c4a08f030c75677d4d66f1d4b54b248
clasindu <lasindura00@gmail.com>
2026-06-17 20:44:45 +0530

    write constructor

--- src/AccountHolder.java
+++ src/AccountHolder.java
@@ -1,2 +1,14 @@
 public class AccountHolder {
+
+    public String name;
+    public String birthday;
+    public String address;
+
+    public AccountHolder(String name , String birthday , String address){
+        this.name     = name;
+        this.address  = address;
+        this.birthday = birthday;
+
+
+    }
 }
