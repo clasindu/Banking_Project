8e4fb13523a231550b2de5156fb5766a531f7a5c
clasindu <lasindura00@gmail.com>
2026-06-18 08:52:16 +0530

    test the code

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args){
 
+        AccountHolder jane =new AccountHolder("Jane Anderson", "06/05/1998" ,"Colombo");
+        System.out.println(jane.name);
+
     }
 }
