4a6ce666151ef75a068bd86f85b2ac3673931b36
clasindu <lasindura00@gmail.com>
2026-06-18 09:51:35 +0530

    create BanKAccount Class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,19 @@
 public class BankAccount {
+
+    public String name;
+    public String birthday;
+    public String address;
+    public String accountNumber;
+    public String accountType;
+    public int balance;
+
+    public BankAccount(String name , String birthday ,String address, String accountNumber , String accountType){
+        this.name        =  name;
+        this.birthday    =  birthday;
+        this.address     =  address;
+        this.accountNumber= accountNumber;
+        this.accountType  = accountType;
+        this.balance      =0;
+
+    }
 }
