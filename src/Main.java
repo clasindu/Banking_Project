dd9eb942418e96f2a042bdb4718d38344e6f6f7e
clasindu <lasindura00@gmail.com>
2026-06-18 12:39:14 +0530

    test and run

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,18 @@ public class Main {
     public static void main(String[] args){
 
         AccountHolder jane =new AccountHolder("Jane Anderson", "06/05/1998" ,"Colombo");
-        System.out.println(jane.name);
-        System.out.println(jane.address);
+//        System.out.println(jane.name);
+//        System.out.println(jane.address);
 
-        BankAccount lasi = new BankAccount("Chamath Lasindu","30/05/2000" ,"Newtown,Ratnapura" ,"2933839389", "sv");
-        System.out.println(lasi.name);
-        System.out.println(lasi.accountNumber);
+        BankAccount lasiBankAccount = new BankAccount("Chamath Lasindu","30/05/2000" ,"Newtown,Ratnapura" ,"2933839389", "sa");
+        lasiBankAccount.depositCash(500);
+        lasiBankAccount.withdrawCash(400);
+
+        System.out.println(lasiBankAccount.getBalance());
+
+
+
+
 
     }
 }
